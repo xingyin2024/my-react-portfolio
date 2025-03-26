81c72cde6ed1e4a5f1980c0614c9ccefd673a3db
xingyin2024 <xingyin.thu@gmail.com>
2025-03-26 14:48:38 +0100

    update article image style

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,19 +1,26 @@
 import articles from "../Libraries/articles.json";
-import { Button } from "./Button/Button"
-
+import { Button } from "./Button/Button";
 
 export const ArticleCard = () => {
   return (
     <>
       {articles.articles.map((item) => (
         <div className="article-card" key={item.id}>
-          <img className="article-img" src={item.image} alt="article image" />
-          
+          <div className="article-image-container">
+            <a href={item.global} target="_blank">
+              <img
+                className="article-image"
+                src={item.image}
+                alt={item.title}
+              />
+            </a>
+          </div>
+
           <div className="description">
-            <p className="date">October 28, 2024</p>
+            <p className="date">{item.date}</p>
             <h3>{item.title}</h3>
             <p>{item.description}</p>
-            
+
             <div className="buttons">
               <Button
                 className="button"
@@ -27,5 +34,5 @@ export const ArticleCard = () => {
         </div>
       ))}
     </>
-  )
-}
+  );
+};
